295167bb55dfc5c3159f3fddfb2f7cbba2fef1f4
yongming <yongming@solarmonkey.nl>
2021-02-11 10:27:24 +0100

    Add gke cluster

---
+++ gke.tf
@@ -0,0 +1,74 @@
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  # Per zone
+  default     = 1
+  description = "number of gke nodes"
+}
+
+resource "google_project_service" "enable-compute-engine" {
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "enable-kubernetes-engine" {
+  service = "container.googleapis.com"
+}
+
+resource "google_project_service" "enable-cloud-resource" {
+  service = "cloudresourcemanager.googleapis.com"
+}
+
+# GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+
+  master_auth {
+    username = var.gke_username
+    password = var.gke_password
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${google_container_cluster.primary.name}-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = var.gke_num_nodes
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    # preemptible  = true
+    machine_type = "e2-medium"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
